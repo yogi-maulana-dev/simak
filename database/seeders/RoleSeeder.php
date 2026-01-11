205251264b6ac6bf6e9fea38c91194cbaced422a
yogi-maulana-dev <yogi.maulana.2021211032@mail.darmajaya.ac.id>
2026-01-11 17:13:51 +0700

    update seeder database

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+         DB::table('roles')->insert([
+            ['name' => 'superadmin'],
+            ['name' => 'admin_univ'],
+            ['name' => 'admin_fakultas'],
+            ['name' => 'admin_prodi'],
+            ['name' => 'asesor_fakultas'],
+            ['name' => 'asesor_prodi'],
+        ]);
+    }
+}
